f3ba080de6c8a574521907866dac822f2ff555ed
Andrey Klimenko <klimandr@mail.ru>
2018-06-26 10:14:13 +0300

    #2 some improvements

--- resources/views/Hero/create.blade.php
+++ resources/views/Hero/create.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
     <div class="container">
-    {!! Form::open(['url' => 'heroes']) !!}
-        <div class="form-group">
-            {!! Form::label('Name:') !!}
-            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+        <h2>Creating a hero ...</h2>
+        <hr>
+        {!! Form::open(['url' => 'heroes']) !!}
+            <div class="form-group">
+                {!! Form::label('Name:') !!}
+                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+            </div>
             {!! Form::submit('Store', ['class' => 'btn btn-success']) !!}
-        </div>
-    {!! Form::close() !!}
+            <a href="{!! URL::previous() !!}" class="btn btn-dark">Back</a>
+        {!! Form::close() !!}
     </div>
 @endsection
